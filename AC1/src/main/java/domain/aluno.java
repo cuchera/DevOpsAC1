d0df4f687a5855c2d288a2e0b04f872de19a84de
Ariane Rossetti <108526841+ArianeRossetti@users.noreply.github.com>
2026-09-09 22:17:00 -0300

    Implementa direto a recuperação para média 7

--- AC1/src/main/java/domain/aluno.java
+++ AC1/src/main/java/domain/aluno.java
@@ -1,6 +1,16 @@
 package domain;
 
-public class aluno {
+public class Aluno {
 
+    private boolean direitoRecuperacao = false;
 
+    public void encerrarCurso(double mediaFinal) {
+        if (mediaFinal == 7.0) {
+            direitoRecuperacao = true;
+        }
+    }
+
+    public boolean getDireitoRecuperacao() {
+        return direitoRecuperacao;
+    }
 }
