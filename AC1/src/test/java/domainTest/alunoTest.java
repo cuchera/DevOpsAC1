98ea40a49f28b57c8a2716c5282917e061a7f8e3
Ariane Rossetti <108526841+ArianeRossetti@users.noreply.github.com>
2026-09-09 22:19:02 -0300

    Add recovery rights test for Aluno classAdiciona teste BDD3 de recuperação

--- AC1/src/test/java/domainTest/alunoTest.java
+++ AC1/src/test/java/domainTest/alunoTest.java
@@ -1,9 +1,18 @@
 package domainTest;
 
+import domain.Aluno;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class alunoTest {
 
     @Test
-    public void
+    public void deveTerDireitoARecuperacaoQuandoMediaForIgualASete() {
+
+        Aluno aluno = new Aluno();
+
+        aluno.encerrarCurso(7.0);
+
+        Assertions.assertTrue(aluno.getDireitoRecuperacao());
+    }
 }
