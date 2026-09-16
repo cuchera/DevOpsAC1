c438e5028d415a349f3e73abf71b7f57917cda58
Luiz Grosso <lu.nunesgr@gmail.com>
2026-09-16 17:35:43 -0300

    BDD 4 - Testes, RED, GREEN e BLUE

--- AC1/src/test/java/domainTest/AlunoTest.java
+++ AC1/src/test/java/domainTest/AlunoTest.java
@@ -3,20 +3,21 @@ package domainTest;
 import domain.Aluno;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AlunoTest {
 
     @Test
-    void deveZerarSaldoAoMatricularTerceiroCursoExtra() {
+    void deveConcederAcessoAoTerceiroCursoExtra() {
 
         Aluno aluno = new Aluno();
         aluno.setCursosExtras(3);
 
         aluno.matricularCursoExtra();
         aluno.matricularCursoExtra();
+
         aluno.matricularCursoExtra();
 
-        assertEquals(0, aluno.getCursosExtras());
+        assertTrue(aluno.temAcessoAoCursoExtra());
     }
 }
