8b4cd70c4e430171b481654c0a8fd12584bdcc9d
Cuchera <gabrielcuchera23@hotmail.com>
2026-09-16 13:54:15 -0300

    cria DTOs de entrada e saída com schemas do Swagger

---
+++ AC1/src/main/java/dto/AlunoRequestDTO.java
@@ -0,0 +1,14 @@
+package dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record AlunoRequestDTO(
+        @Schema(description = "Nome do aluno", example = "Leonardo da Silva")
+        String nome,
+
+        @Schema(description = "Indica se o curso foi concluído", example = "true")
+        boolean cursoConcluido,
+
+        @Schema(description = "Média final obtida no curso", example = "8.5")
+        double mediaFinal
+) {}
---
+++ AC1/src/main/java/dto/AlunoResponseDTO.java
@@ -0,0 +1,23 @@
+package dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record AlunoResponseDTO(
+        @Schema(description = "ID gerado", example = "1")
+        Long id,
+
+        @Schema(description = "Nome do aluno", example = "Leonardo Cuchera")
+        String nome,
+
+        @Schema(description = "Status de conclusão", example = "true")
+        boolean cursoConcluido,
+
+        @Schema(description = "Média final", example = "8.5")
+        double mediaFinal,
+
+        @Schema(description = "Cursos extras concedidos", example = "3")
+        int quantidadeCursosExtras,
+
+        @Schema(description = "Indica direito a cursos extras", example = "true")
+        boolean temDireitoACursosExtras
+) {}
